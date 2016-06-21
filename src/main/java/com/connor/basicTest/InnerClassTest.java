fce3604879c2fb4b61ff9838e041a202764df4b5
connorzeng <ganggang334@163.com>
2016-06-21 10:55:44 +0800

    111

--- src/main/java/com/connor/basicTest/InnerClassTest.java
+++ src/main/java/com/connor/basicTest/InnerClassTest.java
@@ -13,79 +13,9 @@ public class InnerClassTest {
 	    System.out.println(TestI.getSingle().hashCode());
 	    System.out.println(TestI.getSingle().hashCode());
     }
-	public static void main(String[] args) {
-	    
-	    TestI i = new TestI("zenggang", 1);
-	    TestI f = i;
-	    
-	    System.out.println(TestI.getSingle().hashCode());
-	    System.out.println(TestI.getSingle().hashCode());
-	    System.out.println(TestI.getSingle().hashCode());
-	    System.out.println(TestI.getSingle().hashCode());
-	    System.out.println(TestI.getSingle().hashCode());
-    }
 }
 class TestI{
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    private static TestI single = new TestI("hello", 1); 
-    private String name;
-    private int age;
-    
-    public TestI(String name, int age) {
-        super();
-        this.name = name;
-        this.age = age;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public int getAge() {
-        return age;
-    }
-    public void setAge(int age) {
-        this.age = age;
-    }
-    public static TestI getSingle(){
-        return single;
-    }
-
-
-
-
-
-
-
-
-
-
-
     private static TestI single = new TestI("hello", 1); 
     private String name;
     private int age;
