a4ff8b9bbac4217c4dadad88876c9316f4f62a9f
connor <ganggang334@163.com>
2016-06-25 12:51:04 +0800

    hello

---
+++ src/main/java/com/connor/thread/AtomicTest.java
@@ -0,0 +1,12 @@
+package com.connor.thread;
+
+/**
+ * 原子操作测试类
+ * @author connor_zeng
+ */
+public class AtomicTest {
+	
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/main/java/com/connor/thread/ExecutorTest.java
@@ -0,0 +1,19 @@
+package com.connor.thread;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ * 测试Executor框架
+ * @author connor_zeng
+ *
+ */
+public class ExecutorTest {
+	
+	public static void main(String[] args) {
+		
+		Executors.newSingleThreadExecutor();
+		
+	}
+	
+}
