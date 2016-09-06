eb1cbfbc48b62418ab9879028d9000faebf356f0
connor <ganggang334@163.com>
2016-09-06 23:44:09 +0800

    hello branch

--- src/main/java/com/connor/jvm/Animal.java
+++ src/main/java/com/connor/jvm/Animal.java
@@ -7,7 +7,7 @@ public class Animal{
 	public static final int FINAL_VALUE = 123;
 	public static int VALUE = 123;
 	public void finalMethod(String string){
-		
+		//hello
 		System.out.println("find method" + string);
 	}
 	
