b34445421be662b94117ab8e905ccdc8d2e4aac4
Connor Zeng <ganggang334@gmail.com>
2019-06-25 10:28:32 +0800

    FileUploadTest.java

---
+++ src/main/java/com/internet/FileUploadTest.java
@@ -0,0 +1,22 @@
+package com.internet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FileUploadTest {
+	
+	public static void main(String[] args) {
+		upload();
+	}
+
+	private static void upload() {
+		
+		Map<String,Object> bizMap = new HashMap<>();
+		bizMap.put("channel", "2018011");
+		bizMap.put("mchId", "B180801102");
+	}
+	
+	
+	
+	
+}
